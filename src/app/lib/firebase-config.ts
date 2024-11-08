6966451d2adce623d7440031b1873f083626366d
Yoannis Sánchez Soto <yoannisgnw@gmail.com>
2024-11-08 08:20:20 -0500

    chore: Update Firebase configuration with measurement ID

--- src/app/lib/firebase-config.ts
+++ src/app/lib/firebase-config.ts
@@ -1,4 +1,5 @@
 // lib/firebaseConfig.js
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -11,6 +12,7 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 let firebaseApp;
@@ -20,5 +22,6 @@ if (!firebaseApp) {
 
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
+const analytics = getAnalytics(firebaseApp);
 
-export { firebaseApp, auth, db };
+export { firebaseApp, auth, db, analytics };
