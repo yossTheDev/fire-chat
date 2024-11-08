45acf3d7c14c9f5d438d1d12195a31202c2a4c14
Yoannis Sánchez Soto <yoannisgnw@gmail.com>
2024-11-08 10:26:24 -0500

    refactor: Update firebaseConfig.ts to remove unnecessary code

--- src/app/lib/firebaseConfig.ts
+++ src/app/lib/firebaseConfig.ts
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
