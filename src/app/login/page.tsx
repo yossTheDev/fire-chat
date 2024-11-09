2aa070d7ac7e8744644492c05e46ceec7ee5bbed
Yoannis Sánchez Soto <yoannisgnw@gmail.com>
2024-11-09 10:16:57 -0500

    refactor: Update ChatRoom component with improved styling and dark mode support

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen dark:bg-neutral-900">
-      <div className="flex flex-col items-center font-bold text-neutral-900">
+      <div className="flex flex-col items-center font-bold text-neutral-900 dark:text-neutral-300">
         <img
           className="size-24"
           src="/FireChatIcon.svg"
@@ -42,24 +42,35 @@ export default function Page() {
         </h2>
       </div>
 
-      <div className="p-6 max-w-md w-full">
+      <div className="p-6 max-w-md w-full items-center justify-center flex flex-col">
         <p className="mt-4 text-gray-500 text-center">
           Inicia sesión usando tu cuenta de Google
         </p>
         <button
           onClick={handleGoogleLogin}
-          className="mt-6 w-full py-2 px-4 flex items-center justify-center gap-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition duration-300"
+          className="mt-6 w-fit py-2 px-4 flex items-center justify-center gap-2 bg-neutral-200 dark:bg-neutral-100 text-neutral-900 font-semibold rounded-full hover:bg-neutral-500/40 dark:hover:bg-neutral-300 transition duration-300"
         >
           <svg
-            className="size-5"
-            role="img"
-            viewBox="0 0 24 24"
+            className="size-6"
+            viewBox="0 0 256 262"
             xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="xMidYMid"
           >
-            <title>Google</title>
             <path
-              fill="currentColor"
-              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+              d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
+              fill="#4285F4"
+            />
+            <path
+              d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"
+              fill="#34A853"
+            />
+            <path
+              d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"
+              fill="#FBBC05"
+            />
+            <path
+              d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"
+              fill="#EB4335"
             />
           </svg>
           Continuar con Google
